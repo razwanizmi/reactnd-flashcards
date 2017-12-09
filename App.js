ebe62031a023503f2797aede9ccfc073fdb0d032
Razwan Rashidi <razwan.izmi@gmail.com>
2017-12-09 11:20:24 +0800

    Set up basic reducer

--- App.js
+++ App.js
@@ -1,13 +1,23 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { Provider } from "react-redux";
+import { combineReducers, createStore } from "redux";
 import { TopBar } from "./components";
+
+const store = createStore(
+  combineReducers({
+    state: (state = {}) => state
+  })
+);
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={{ flex: 1 }}>
-        <TopBar barStyle="light-content" backgroundColor="#7f8c8d" />
-      </View>
+      <Provider store={store}>
+        <View style={{ flex: 1 }}>
+          <TopBar barStyle="light-content" backgroundColor="#7f8c8d" />
+        </View>
+      </Provider>
     );
   }
 }
